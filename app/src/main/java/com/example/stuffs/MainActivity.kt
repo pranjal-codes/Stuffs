9dfc9f4ead8e44b77dcd319968a93b18d9e533a0
Pranjal <mpranjal05@gmail.com>
2021-03-13 13:24:00 +0530

    Added Data Repository and Data ViewModel

--- app/src/main/java/com/example/stuffs/MainActivity.kt
+++ app/src/main/java/com/example/stuffs/MainActivity.kt
@@ -15,9 +15,10 @@ import com.example.stuffs.databinding.ItemNoteBinding
 import com.google.android.material.snackbar.Snackbar
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), INotesAdapter {
     private lateinit var binding: ActivityMainBinding
     private lateinit var noteBinding: ItemNoteBinding
+    private lateinit var viewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +26,46 @@ class MainActivity : AppCompatActivity() {
         noteBinding = ItemNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.noteRecyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = NotesAdapter(this, this)
+        binding.noteRecyclerView.adapter = adapter
+
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(NoteViewModel::class.java)
+        viewModel.allNotes.observe(this, Observer { list ->
+            list?.let {
+                adapter.updateList(it)
+            }
+        })
+
     }
 
-
+    override fun onItemClicked(note: Note) {
+        var temp = note
+        viewModel.deleteNote(note)
+        Snackbar.make(binding.root,"Note Deleted", Snackbar.LENGTH_LONG).setAction("Undo") {
+            viewModel.insertNote(temp)
+            binding.inputBox.text.clear();
+        }.setBackgroundTint(Color.parseColor("#202020")).setActionTextColor(Color.GREEN).setTextColor(Color.LTGRAY).show()
+    }
 
     fun addNote(view: View) {
-        TODO("Not yet implemented")
+        val noteText = binding.inputBox.text.toString()
+        if (noteText.isNotEmpty()) {
+            viewModel.insertNote(Note(noteText))
+            binding.inputBox.text.clear();
+        } else {
+            binding.inputBox.startAnimation(shakeError())
+        }
     }
 
-
+    private fun shakeError(): Animation {
+        val shake = TranslateAnimation(0f, 10f, 0f, 0f)
+        shake.duration = 250
+        CycleInterpolator(5F).also { shake.interpolator = it }
+        return shake
+    }
 }
